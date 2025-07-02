9c51d8043838f7bd93b972b9d38d0819f8e16d77
Himanshu Pandey <himanshup01428@gmail.com>
2025-07-02 19:11:04 +0530

    added schema

---
+++ db.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+mongoose.connect(
+  "mongodb+srv://admin:HoK9Sx7Lzq2eeiRD@cluster0.pdzqudi.mongodb.net/coursera-app"
+);
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.ObjectId;
+
+const userSchema = new Schema({
+  userId: ObjectId,
+  email: { type: String, unique: true },
+  password: String,
+  firstName: String,
+  lastName: String,
+});
+const adminSchema = new Schema({
+  email: { type: String, unique: true },
+  password: String,
+  firstName: String,
+  lastName: String,
+});
+const courseSchema = new Schema({
+  title: String,
+  description: String,
+  imageUrl: String,
+  price: Number,
+  createrID: ObjectId,
+});
+const purchasesSchema = new Schema({
+  userId: ObjectId,
+  courseId: ObjectId,
+});
+
+const userModel = mongoose.model("user", userSchema);
+const adminModel = mongoose.model("admin", adminSchema);
+const courseModel = mongoose.model("course", courseSchema);
+const purchaseModel = mongoose.model("purchases", purchasesSchema);
+
+module.exports = {
+  userModel,
+  adminModel,
+  courseModel,
+  purchaseModel,
+};
