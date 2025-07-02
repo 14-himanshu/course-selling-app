bdfc5cafa81c7cbccc2e8c3f7fe50109e6ab2d9d
Himanshu Pandey <himanshup01428@gmail.com>
2025-07-02 14:00:15 +0530

    Step 2 added routes

--- index.js
+++ index.js
@@ -1,30 +1,10 @@
 const express = require('express')
+const { userRouter } = require('./routes/user')
+const {courseRouter} = require("./routes/course")
 const app = express()
 
-app.post("/user/signup",function(req,res){
-    res.json({
-        message : "signup endpoint"
-    })
-})
-app.post("/user/signin",function(req,res){
-    res.json({
-        message : "signin endpoint"
-    })
-})
-app.get("/courses/purchases",function(req,res){
-    res.json({
-        message : "signup endpoint"
-    })
-})
-app.post("/user/purchase")
-app.get("/courses",function(req,res){
-    res.json({
-        message : "signup endpoint"
-    })
-})
-
-
-
+app.use('/user',userRouter)
+app.use('/course',courseRouter)
 
 
 
